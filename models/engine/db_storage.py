0bf39da49facde913e5349f4858a6ccf8c7235c0
bonface murangiri <bonnyrangi95@gmail.com>
2024-01-07 19:53:55 +0000

    adding retrieve and count method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,19 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def get(self, cls, id):
+        """method to retrieve one object"""
+        objects = all(cls)
+        for obj in objects.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        objects = self.all(cls)
+        return len(objects)
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,6 +40,19 @@ class FileStorage:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
+    def get(self, cls, id):
+        """method to retrieve one object"""
+        objects = all(cls)
+        for obj in objects.values():
+            if obj.id = id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage."""
+        objects = self.all(cls)
+        return len(objects)
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
