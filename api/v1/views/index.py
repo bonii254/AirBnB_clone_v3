8743c726bc8a3fac465662f7afa395b46f515592
otiaudi <otiaudi9@gmail.com>
2024-01-09 00:22:38 +0300

    updated api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" Index """
+"""Index of views"""
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """ Returns JSON """
-    return jsonify(status="OK")
+    """Return the status of the api."""
+    return jsonify({"status": "OK"}), 200
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
-    """ Returns the number of each instance type """
-    return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+    """endpoint that retrieves the number of each objects by type"""
+    obj_counts = {}
+    objects = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users",
+    }
+
+    for key, value in objects.items():
+        obj_counts[value] = storage.count(key)
+    return jsonify(obj_counts), 200
